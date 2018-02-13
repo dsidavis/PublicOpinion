6ef4ef0670dc667f9cce9af82f5221452967bdbd
Matt Espe <mespe@ucdavis.edu>
2018-02-13 10:16:31 -0800

    Adds function to aggregate the frame data into a time series.

--- R/ts_utils.R
+++ R/ts_utils.R
@@ -1,6 +1,7 @@
 
 
 getSurges = function(df, threshold)
+    # Not used - maybe move to metrics.R
 {
     by(df, list(df$top_frame, df$tone), function(x, threshold){
         x = x[order(x$interval),]
@@ -22,3 +23,28 @@ byInterval = function(df, interval)
     byX$Count[byX$tone == "Anti"] = byX$Count[byX$tone == "Anti"] * -1
     byX
 }
+
+aggregateArticles = function(df, interval = "week")
+    #
+    #
+    # 
+{
+    if(!interval %in% c("day", "week", "month", "year"))
+        stop("Interval must be one of: 'day', 'week', 'month', or 'year'")
+
+    ans = by(df, cut(df$date, interval), function(x) {
+        data.frame(startDate = min(x$date),
+                   endDate = max(x$date),
+                   avgPro = mean(x$Pro[x$tone == "Pro"], na.rm = TRUE),
+                   nPro = sum(x$tone == "Pro"),
+                   avgNeut = mean(x$Neutral[x$tone == "Neutral"], na.rm = TRUE),
+                   nPro = sum(x$tone == "Neutral"),
+                   avgAnti = mean(x$Anti[x$tone == "Anti"], na.rm = TRUE),
+                   nAnti = sum(x$tone == "Anti"),
+                   nSource = length(unique(x$Source)),
+                   nTotal = nrow(x))
+    })
+    do.call(rbind, ans)
+}
+
+
